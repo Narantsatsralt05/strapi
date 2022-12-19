534452d6c4f275c7b8cde46dbab95a751ca09112
Narantsatsralt05 <n.bumnasan@gmail.com>
2022-12-19 15:09:19 +0800

    Changed plugins config

--- config/plugins.js
+++ config/plugins.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     config: {
       deployHook: `https://api.vercel.com/v1/integrations/deploy/prj_${process.env.VERCEL_DEPLOY_PLUGIN_HOOK}`,
       apiToken: process.env.VERCEL_DEPLOY_PLUGIN_API_TOKEN,
-      appFilter: "admin",
+      appFilter: process.env.VERCEL_DEPLOY_PLUGIN_APP_FILTER,
       roles: ["strapi-super-admin"],
     },
   },
